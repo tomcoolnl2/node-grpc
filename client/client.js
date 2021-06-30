949ad19382f046007ed593acb08f038dbf60c955
Tom Cool <tom.cool@indivirtual.com>
2021-06-30 13:11:31 +0200

    dynamically load proto/package definition

--- client/client.js
+++ client/client.js
@@ -1,28 +1,40 @@
 
-const grpc = require('grpc')
-const { Calculator, CalculateRequest } = require('../server/protos/calculate_pb')
-const { CalculateServiceClient } = require('../server/protos/calculate_grpc_pb')
+const path = require('path')
+const protoLoader = require('@grpc/proto-loader')
+const { loadPackageDefinition, credentials } = require('grpc')
 
+
+const protoPath = path.join(__dirname, '..', 'protos', 'calculate.proto')
+const protoDefinition = protoLoader.loadSync(protoPath, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+})
+
+const { CalculateService } = loadPackageDefinition(protoDefinition).calculate
+
+const client = new CalculateService(
+    'localhost:50051',
+    credentials.createInsecure()
+)
 
 function main() {
     
-    const client = new CalculateServiceClient(
-        'localhost:50051',
-        grpc.credentials.createInsecure()
-    )
 
-    const request = new CalculateRequest()
 
-    const calculation = new Calculator()
-    calculation.setA(5)
-    calculation.setB(2)
-    
-    request.setCalculation(calculation)
+    const request = {
+        calculation: {
+            a: 1,
+            b: 6
+        }
+    }
     
     client.calculate(request, (error, response) => {
 
         if (!error) {
-            console.log('response', response.getResult())
+            console.log('response', response.result)
         }
         else {
             console.error(error)
--- server/index.js
+++ server/index.js
@@ -1,22 +1,29 @@
 
-const { Server, ServerCredentials } = require('grpc')
-// const { GreetResponse } = require('../server/protos/greet_pb')
-// const { GreetServiceService } = require('../server/protos/greet_grpc_pb')
-const { CalculateResponse } = require('../server/protos/calculate_pb')
-const { CalculateServiceService } = require('../server/protos/calculate_grpc_pb')
+const path = require('path')
+const protoLoader = require('@grpc/proto-loader')
+const { loadPackageDefinition, Server, ServerCredentials } = require('grpc')
+
+const protoPath = path.join(__dirname, '..', 'protos', 'calculate.proto')
+const protoDefinition = protoLoader.loadSync(protoPath, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+})
+
+const { CalculateService } = loadPackageDefinition(protoDefinition).calculate
 
 
 function calculate({ request }, callback) {
-    const response = new CalculateResponse()
-    const calculation = request.getCalculation()
-    response.setResult(calculation.getA() + calculation.getB())
-    callback(null, response)
+    const { a, b } = request.calculation
+    callback(null, { result: a + b })
 }
 
 function main() {
 
     const server = new Server()
-    server.addService(CalculateServiceService, { calculate })
+    server.addService(CalculateService.service, { calculate })
     server.bind('127.0.0.1:50051', ServerCredentials.createInsecure())
     server.start()
 
